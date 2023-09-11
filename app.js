812f43749fcd5d487ef524fa6277e738275f94ab
Tahaansaribinjaved <iamtahaansari293@gmail.com>
2023-09-11 17:58:57 +0500

    lab work

---
+++ app.js
@@ -0,0 +1,151 @@
+// questiion 1
+
+// var a =prompt('please Enter your name') 
+// document.write('Hello' +' '+ a)
+
+
+
+// question 2
+
+// var userInp = +prompt('Enter a number to print its multiplication table')
+// for (i = 1; i <= 10; i++) {
+//     document.write(userInp + " x " + i + " = " + userInp * i + '<br>')
+// }
+
+
+// question 3
+
+// var city = +prompt('Please enter your city')
+
+// if(city = "Karachi"){
+//     document.write('Wellcome to city of lights')
+
+// }
+
+
+
+// question 4
+
+
+// var gender = prompt('Enter your gender')
+
+// if(gender == 'Male'){
+//     document.write('Good morning Sir')
+// }
+// else if(gender == 'Female'){
+//     document.write('Good morning Maam')
+// }
+
+
+// question 5
+
+
+// var traffic = prompt('color')
+
+
+// if (traffic == 'Red'){
+//     document.write('vehicles must stop')
+// }
+
+// else if (traffic == 'Yellow'){
+//     document.write('vehicles should get ready to move ')
+// }
+// else if (traffic == 'Green'){
+//     document.write('vehicles can move now')
+// }
+
+
+// question 6
+
+// var a = +prompt('Enter age')
+// var b = 18
+// if (a<=b){
+//     document.write('You are wellcome')
+// }
+
+// question 7
+
+// var a = +prompt('Enter Fuel')
+// var b = 0.25
+// if (a<b){
+//     document.write('Please refill the fuel in your car ')
+// }
+
+
+// question 8
+
+//   a.
+
+// var a = 4
+// if (++a ===5){
+//     alert('Given conditon for variable a is true');
+// }
+//                                                                chal gaya 
+
+
+//   b.
+// var b = 82;
+// if(b++ === 83){
+//     alert('given condition for variable b is true');
+// }
+
+
+//                                                               nahi chala
+
+
+//  c.
+// var c = 12;
+// if (c++ === 13){
+//     alert('cndition 1 is true');
+// }
+// //                                                               nahi chala
+// if (c === 13){
+//     alert('cndition 2 is true');
+// }
+//                                                                nahi chala
+
+
+// if (++c < 13){
+//     alert('cndition 3 is true');
+// }
+
+
+//                                                                nahi chala
+
+// if (c === 14){
+//     alert('cndition 4 is true');
+// }
+
+//                                                                nahi chala
+// d.
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+//  alert("The cost equals");
+// }
+
+//                                                                chal gaya 
+//  e.
+// if (true){
+//     alert("True");
+//    }
+//    if (false){
+//     alert("False");
+//    }
+
+//                                                                chal gaya 
+
+//  f.
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+//    }
+
+
+//                                                                chal gaya 
+
+
+var firstSub = +prompt('Enter first subject ')
+var secondSub = +prompt('Enter second subject ')
+var thirdSub = +prompt('Enter third subject ')
+var total = firstSub+ secondSub + thirdSub
